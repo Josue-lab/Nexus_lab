5386c1ffba455b0d74b262a8f5337c926b636414
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-02-17 15:05:45 -0800

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -v -u admin:admin123 --upload-file file.txt http://nexus-okdswalab.3.134.232.139.nip.io/content/repositories/kiosk/
+curl -v -u admin:admin123 --upload-file file.txt http://localhost:8081/repository/maven-releases/
